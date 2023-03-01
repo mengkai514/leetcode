ec8a7f9c3f007d5ec42e936ced373638a6cc7447
MK <2608074326@qq.com>
2023-03-01 17:04:55 +0800

    27. 移除元素

--- code/src/array/RemoveElement/RemoveElement.java
+++ code/src/array/RemoveElement/RemoveElement.java
@@ -4,6 +4,7 @@ package array.RemoveElement;
  * 27. 移除元素
  */
 public class RemoveElement {
+
     public int removeElement(int[] nums, int val) {
         int slowIndex = 0;
         for (int fastIndex = 0; fastIndex < nums.length; fastIndex++){
