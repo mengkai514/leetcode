2ac2ef9f7cbc0e35d87976b62db62107213f246f
MK <2608074326@qq.com>
2023-03-01 17:05:36 +0800

    977. 有序数组的平方

--- code/src/array/SquareOfOrderedArray/Square.java
+++ code/src/array/SquareOfOrderedArray/Square.java
@@ -4,6 +4,7 @@ package array.SquareOfOrderedArray;
  * 977. 有序数组的平方
  */
 public class Square {
+
     public int[] sortedSquares(int[] nums) {
         int[] result = new int[nums.length];
         int k = nums.length - 1;
