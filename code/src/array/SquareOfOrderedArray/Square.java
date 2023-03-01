a451b939974b52698f77ed1fb5d5f865ff1036cc
MK <2608074326@qq.com>
2023-03-01 15:57:57 +0800

    有序数组的平方

---
+++ code/src/array/SquareOfOrderedArray/Square.java
@@ -0,0 +1,23 @@
+package array.SquareOfOrderedArray;
+
+/**
+ * 977. 有序数组的平方
+ */
+public class Square {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int k = nums.length - 1;
+        int j = nums.length - 1;
+        int i = 0;
+        while (i <= j){
+            if (nums[i] * nums[i] < nums[j] * nums[j]) {
+                result[k--] = nums[j] * nums[j];
+                j--;
+            } else {
+                result[k--] = nums[i] * nums[i];
+                i++;
+            }
+        }
+        return result;
+    }
+}
