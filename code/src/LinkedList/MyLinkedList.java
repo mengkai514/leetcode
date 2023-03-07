06d12c45a78328895d0dec11776a021cbb28db42
MK <2608074326@qq.com>
2023-03-07 09:31:34 +0800

    707. 设计链表

---
+++ code/src/LinkedList/MyLinkedList.java
@@ -0,0 +1,81 @@
+package LinkedList;
+
+/**
+ * 707. 设计链表
+ */
+public class MyLinkedList {
+    //size存储链表元素的个数
+    int size;
+    //虚拟头结点
+    ListNode head;
+
+    //初始化链表
+    public MyLinkedList() {
+        size = 0;
+        head = new ListNode(0);
+    }
+
+    public int get(int index) {
+        if (index < 0 && index >= size) {
+            return -1;
+        }
+        ListNode currentNode = head;
+        //包含一个虚拟头节点，所以查找第 index+1 个节点
+        for (int i = 0; i <= index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode.val;
+    }
+
+    //在链表最前面插入一个节点，等价于在第0个元素前添加
+    public void addAtHead(int val) {
+        addAtIndex(0,val);
+    }
+
+    //在链表的最后插入一个节点，等价于在(末尾+1)个元素前添加
+    public void addAtTail(int val) {
+        addAtIndex(size,val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+        index = Math.max(0, index);
+        size++;
+        ListNode pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred.next;
+        }
+        ListNode toAdd = new ListNode(val);
+        toAdd.next = pred.next;
+        pred.next = toAdd;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+        size--;
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+        ListNode pred = head;
+        for (int i = 0; i < index ; i++) {
+            pred = pred.next;
+        }
+        pred.next = pred.next.next;
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList my = new MyLinkedList();
+        my.addAtHead(1);
+        my.addAtTail(3);
+        my.addAtIndex(1,2);   //链表变为1-> 2-> 3
+        my.addAtHead(5);
+        my.get(1);            //返回2
+        my.deleteAtIndex(1);  //现在链表是1-> 3
+        my.get(1);            //返回3
+    }
+}
