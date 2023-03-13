8c9c5840d36d60691de7b76f0456717d2d4c361c
MK <2608074326@qq.com>
2023-03-13 20:05:31 +0800

    344. 反转字符串

--- code/src/String/ReverseString.java
+++ code/src/String/ReverseString.java
@@ -4,12 +4,7 @@ package String;
  * 344. 反转字符串
  */
 public class ReverseString {
-    public static void main(String[] args) {
-        String s = "hello";
-        reverseString(s.toCharArray());
-        System.out.println(s);
-    }
-    public static void reverseString(char[] s) {
+    public void reverseString(char[] s) {
         int left = 0;
         int right = s.length - 1;
         while (left < right) {
