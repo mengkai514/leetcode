421d7bec426d4a44f6957f541382889a14a0db11
MK <2608074326@qq.com>
2023-03-13 11:07:56 +0800

    142. 环形链表 II

--- code/src/LinkedList/DetectCycle.java
+++ code/src/LinkedList/DetectCycle.java
@@ -7,6 +7,19 @@ public class DetectCycle {
     public ListNode detectCycle(ListNode head) {
         ListNode fastIndex = head;
         ListNode slowIndex = head;
-
+        while (fastIndex != null && fastIndex.next != null) {
+            fastIndex = fastIndex.next.next;
+            slowIndex = slowIndex.next;
+            if (fastIndex == slowIndex) {
+                ListNode index1 = fastIndex;
+                ListNode index2 = head;
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index2;
+            }
+        }
+        return null;
     }
 }
