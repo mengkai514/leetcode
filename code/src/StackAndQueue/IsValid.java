efd437cd7a8cf25cbf69fd0cdff922158a75a478
MK <2608074326@qq.com>
2023-03-16 15:12:36 +0800

    20. 有效的括号

---
+++ code/src/StackAndQueue/IsValid.java
@@ -0,0 +1,34 @@
+package StackAndQueue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 20. 有效的括号
+ */
+public class IsValid {
+    public static void main(String[] args) {
+        System.out.println(isValid("({{}])"));
+    }
+    public static boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        char ch;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            //碰到左括号，就把相应的右括号入栈
+            if (ch == '(') {
+                deque.push(')');
+            }else if (ch == '{') {
+                deque.push('}');
+            }else if (ch == '[') {
+                deque.push(']');
+            } else if (deque.isEmpty() || deque.peek() != ch) {
+                return false;
+            }else {//如果是右括号判断是否和栈顶元素匹配
+                deque.pop();
+            }
+        }
+        //最后判断栈中元素是否匹配
+        return deque.isEmpty();
+    }
+}
