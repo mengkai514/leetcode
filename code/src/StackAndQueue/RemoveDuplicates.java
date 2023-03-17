d333e3c2a326315b2212ac54a1c94478f4d36dfe
MK <2608074326@qq.com>
2023-03-17 10:01:12 +0800

    1047. 删除字符串中的所有相邻重复项

---
+++ code/src/StackAndQueue/RemoveDuplicates.java
@@ -0,0 +1,30 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+/**
+ * 1047. 删除字符串中的所有相邻重复项
+ */
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("abbaca"));
+    }
+    public static String removeDuplicates(String s) {
+        Stack<Character> stack =new Stack<>();
+        char ch;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            if (stack.isEmpty() || stack.peek() != ch) {
+                stack.push(ch);
+            } else {
+                stack.pop();
+            }
+        }
+        String str = "";
+        //剩余的元素即为不重复的元素
+        while (!stack.isEmpty()) {
+            str = stack.pop() + str;
+        }
+        return str;
+    }
+}
