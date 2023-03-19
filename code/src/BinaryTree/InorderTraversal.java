32b4a46ed6cae4088ece2a1e8a1821c18026e69c
MK <2608074326@qq.com>
2023-03-19 15:34:42 +0800

    方法三，递归

--- code/src/BinaryTree/InorderTraversal.java
+++ code/src/BinaryTree/InorderTraversal.java
@@ -54,4 +54,33 @@ public class InorderTraversal {
         }
         return result;
     }
+
+    //方法三，递归
+    public static List<Integer> inorderTraversal3(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root,result);
+        return result;
+    }
+
+    private static void inorder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left,result);
+        result.add(root.val);
+        inorder(root.right,result);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        TreeNode left1 = new TreeNode(4);
+        TreeNode right1 = new TreeNode(6);
+        TreeNode left2 = new TreeNode(1);
+        TreeNode right2 = new TreeNode(2);
+        root.left = left1;
+        root.right = right1;
+        left1.left = left2;
+        left1.right = right2;
+        System.out.println(inorderTraversal3(root));
+    }
 }
