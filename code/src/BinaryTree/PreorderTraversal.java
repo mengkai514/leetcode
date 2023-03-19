49fa5aeb3a376bddc3f48532f87a2dbca3e1015b
MK <2608074326@qq.com>
2023-03-19 14:29:41 +0800

    144. 二叉树的前序遍历

---
+++ code/src/BinaryTree/PreorderTraversal.java
@@ -0,0 +1,32 @@
+package BinaryTree;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 144. 二叉树的前序遍历
+ */
+public class PreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            result.add(temp.val);
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
+        return result;
+    }
+}
