e04b2fef3d925e3759a3eaa9de54e33e9c6522ae
MK <2608074326@qq.com>
2023-03-31 16:11:14 +0800

    501. 二叉搜索树中的众数

---
+++ code/src/BinaryTree/FindMode.java
@@ -0,0 +1,52 @@
+package BinaryTree;
+
+import java.util.*;
+
+/**
+ * 501. 二叉搜索树中的众数
+ */
+public class FindMode {
+    ArrayList<Integer> resList;
+    int maxCount;
+    int count;
+    TreeNode pre;
+
+    public int[] findMode(TreeNode root) {
+        resList = new ArrayList<>();
+        maxCount = 0;
+        count = 0;
+        pre = null;
+        findMode1(root);
+        int[] res = new int[resList.size()];
+        for (int i = 0; i < resList.size(); i++) {
+            res[i] = resList.get(i);
+        }
+        return res;
+    }
+
+    public void findMode1(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        findMode1(root.left);
+
+        int rootValue = root.val;
+        // 计数
+        if (pre == null || rootValue != pre.val) {
+            count = 1;
+        } else {
+            count++;
+        }
+        // 更新结果以及maxCount
+        if (count > maxCount) {
+            resList.clear();
+            resList.add(rootValue);
+            maxCount = count;
+        } else if (count == maxCount) {
+            resList.add(rootValue);
+        }
+        pre = root;
+
+        findMode1(root.right);
+    }
+}
