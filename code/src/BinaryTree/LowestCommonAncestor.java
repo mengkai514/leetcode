877b47b65a1ff364570cb41ce456bd319abd1740
MK <2608074326@qq.com>
2023-03-31 17:06:31 +0800

    236. 二叉树的最近公共祖先

---
+++ code/src/BinaryTree/LowestCommonAncestor.java
@@ -0,0 +1,41 @@
+package BinaryTree;
+
+/**
+ * 236. 二叉树的最近公共祖先
+ */
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        } else if (right != null && left == null) {
+            return right;
+        } else if (right == null && left != null) {
+            return left;
+        } else return null;
+    }
+
+    //235. 二叉搜索树的最近公共祖先
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root.val > p.val && root.val > q.val) {
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            if (left != null) {
+                return left;
+            }
+        }
+        if (root.val < p.val && root.val < q.val) {
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (right != null) {
+                return right;
+            }
+        }
+        return root;
+    }
+}
