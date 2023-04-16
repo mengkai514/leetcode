260129868d5f8ccf02253266d5e82bfe831b3473
MK <2608074326@qq.com>
2023-04-16 20:27:08 +0800

    134. 加油站

---
+++ code/src/Greedy/CanCompleteCircuit.java
@@ -0,0 +1,24 @@
+package Greedy;
+
+/**
+ * 134. 加油站
+ */
+public class CanCompleteCircuit {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int curSum = 0;
+        int totalSum = 0;
+        int start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            curSum += gas[i] - cost[i];
+            totalSum += gas[i] - cost[i];
+            if (curSum < 0) {
+                start = i + 1;
+                curSum = 0;
+            }
+        }
+        if (totalSum < 0) {
+            return -1;
+        }
+        return start;
+    }
+}
