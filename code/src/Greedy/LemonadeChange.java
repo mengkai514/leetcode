749f89161b4e6bfa50dba95ccf0bb531d8abd1ca
MK <2608074326@qq.com>
2023-04-17 19:25:51 +0800

    860. 柠檬水找零

---
+++ code/src/Greedy/LemonadeChange.java
@@ -0,0 +1,30 @@
+package Greedy;
+
+/**
+ * 860. 柠檬水找零
+ */
+public class LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                five--;
+                ten++;
+            } else if (bill == 20) {
+                if (ten > 0) {
+                    ten--;
+                    five--;
+                } else {
+                    five -= 3;
+                }
+            }
+            if (five < 0 || ten < 0) return false;
+        }
+
+        return true;
+    }
+}
