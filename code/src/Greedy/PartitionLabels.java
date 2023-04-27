bea8a4d5125bbff20b9bc651f99991f3e05a9b12
MK <2608074326@qq.com>
2023-04-27 16:36:53 +0800

    763. 划分字母区间

---
+++ code/src/Greedy/PartitionLabels.java
@@ -0,0 +1,33 @@
+package Greedy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 763. 划分字母区间
+ * @author MK
+ * @date 2023/4/25 - 10:40
+ */
+public class PartitionLabels {
+    public static void main(String[] args) {
+        System.out.println(partitionLabels("ababcbacadefegdehijhklij"));
+    }
+    public static List<Integer> partitionLabels(String s) {
+        List<Integer> list = new LinkedList<>();
+        int[] edge = new int[26];
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            edge[chars[i] - 'a'] = i;
+        }
+        int idx = 0;
+        int last = -1;
+        for (int i = 0; i < chars.length; i++) {
+            idx = Math.max(idx,edge[chars[i] - 'a']);
+            if (i == idx) {
+                list.add(i - last);
+                last = i;
+            }
+        }
+        return list;
+    }
+}
