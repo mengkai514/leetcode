6760b15763df78feaf2552f39e659fb68e7ed191
mengkai <2608074326@qq.com>
2023-04-28 11:32:48 +0800

    738.单调递增的数字

---
+++ code/src/Greedy/MonotoneIncreasingDigits.java
@@ -0,0 +1,24 @@
+package Greedy;
+
+/**
+ * 738.单调递增的数字
+ * @author MK
+ * @date 2023/4/28 - 10:31
+ */
+public class MonotoneIncreasingDigits {
+    public int monotoneIncreasingDigits(int n) {
+        String s = String.valueOf(n);
+        char[] chars = s.toCharArray();
+        int start = s.length();
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (chars[i] > chars[i + 1]) {
+                chars[i]--;
+                start = i + 1;
+            }
+        }
+        for (int i = start; i < s.length(); i++) {
+            chars[i] = '9';
+        }
+        return Integer.parseInt(String.valueOf(chars));
+    }
+}
