76764b4b3ad32b8be6e83493763e5fcab0bcd6ad
mengkai <2608074326@qq.com>
2023-05-05 20:58:40 +0800

    63. 不同路径 II

--- code/src/DP/UniquePaths.java
+++ code/src/DP/UniquePaths.java
@@ -6,6 +6,13 @@ package DP;
  * 62. 不同路径
  */
 public class UniquePaths {
+    /**
+     * 1. 确定dp数组下标含义 dp[i][j] 到每一个坐标可能的路径种类
+     * 2. 递推公式 dp[i][j] = dp[i-1][j] dp[i][j-1]
+     * 3. 初始化 dp[i][0]=1 dp[0][i]=1 初始化横竖就可
+     * 4. 遍历顺序 一行一行遍历
+     * 5. 推导结果
+     */
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         dp[0][0] = 1;
