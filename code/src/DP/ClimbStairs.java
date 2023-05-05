592e025e58ce960b9a4aba86305094e0d10c2dd2
mengkai <2608074326@qq.com>
2023-05-05 15:46:17 +0800

    70. 爬楼梯

---
+++ code/src/DP/ClimbStairs.java
@@ -0,0 +1,24 @@
+package DP;
+
+/**
+ * @author MK
+ * @date 2023/5/5 - 15:19
+ * 70. 爬楼梯
+ */
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
