438e93a46abd337249ca45899ffc1efb004b50dc
mengkai <2608074326@qq.com>
2023-05-08 16:01:30 +0800

    494. 目标和

---
+++ code/src/DP/FindTargetSumWays.java
@@ -0,0 +1,26 @@
+package DP;
+
+/**
+ * @author MK
+ * @date 2023/5/8 - 15:36
+ * 494. 目标和
+ */
+public class FindTargetSumWays {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) sum += nums[i];
+        //如果target过大 sum将无法满足
+        if ( target < 0 && sum < -target) return 0;
+        if ((target + sum) % 2 != 0) return 0;
+        int size = (target + sum) / 2;
+        if(size < 0) size = -size;
+        int[] dp = new int[size + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = size; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[size];
+    }
+}
