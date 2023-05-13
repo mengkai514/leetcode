df6a90c4769f89e6d5ce1a6e4ce13565e52f4dab
mengkai <2608074326@qq.com>
2023-05-13 15:10:07 +0800

    多重背包问题

---
+++ code/src/DP/MultiPack.java
@@ -0,0 +1,36 @@
+package DP;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author MK
+ * @date 2023/5/13 - 15:01
+ * 多重背包问题
+ */
+public class MultiPack {
+    public static void main(String[] args) {
+        // 版本一：改变物品数量为01背包格式
+        List<Integer> weight = new ArrayList<>(Arrays.asList(1, 3, 4));
+        List<Integer> value = new ArrayList<>(Arrays.asList(15, 20, 30));
+        List<Integer> nums = new ArrayList<>(Arrays.asList(2, 3, 2));
+        int bagWeight = 10;
+
+        for (int i = 0; i < nums.size(); i++) {
+            while (nums.get(i) > 1) { // 把物品展开为i
+                weight.add(weight.get(i));
+                value.add(value.get(i));
+                nums.set(i, nums.get(i) - 1);
+            }
+        }
+
+        int[] dp = new int[bagWeight + 1];
+        for(int i = 0; i < weight.size(); i++) { // 遍历物品
+            for(int j = bagWeight; j >= weight.get(i); j--) { // 遍历背包容量
+                dp[j] = Math.max(dp[j], dp[j - weight.get(i)] + value.get(i));
+            }
+            System.out.println(Arrays.toString(dp));
+        }
+    }
+}
